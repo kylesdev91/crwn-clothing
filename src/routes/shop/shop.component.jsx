3a001217fb1d325fc33b7a421ceb0893d3ef35d1
Kyle Hernandez <kh2kt@kylesdev91yahoo.onmicrosoft.com>
2022-06-04 10:57:46 -0500

    REACT LECTURE 132

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 
 import CategoriesPreview from '../categories-preview/categories-preview.component';
-import Category from '../category/category.component';
 
 import './shop.styles.scss';
 
@@ -9,7 +8,6 @@ const Shop = () => {
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />
-      <Route path=':category' element={<Category />} />
     </Routes>
   );
 };
