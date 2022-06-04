236fc856eb1444b4f5e22bebe913c1be9c91cea7
Kyle Hernandez <kh2kt@kylesdev91yahoo.onmicrosoft.com>
2022-06-04 09:19:06 -0500

    REACT Lecture 127

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,6 +1,6 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
-import SHOP_DATA from '../shop-data.js';
+import { addCollectionAndDocuments } from '../utils/firebase/firebase.utils.js';
 
 export const ProductsContext = createContext({
   products: [],
