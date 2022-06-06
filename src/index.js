0f75ee41f348e3fe2b356c9d65c72d77103d0487
Kyle Hernandez <kh2kt@kylesdev91yahoo.onmicrosoft.com>
2022-06-05 20:50:42 -0500

    REACT Lecture 151!!!*

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 import App from './App';
 import { UserProvider } from './contexts/user.context';
@@ -12,15 +14,17 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <UserProvider>
+          <CategoriesProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </CategoriesProvider>
+        </UserProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,10 @@
-import { compose, createStore, applyMiddleWear } from 'redux';
+import { compose, createStore, applyMiddleware } from 'redux';
 import { logger } from 'redux-logger';
 
-// root-reducer: combination of all reducers
+import { rootReducer } from './root-reducer';
+
+const middleWares = [logger];
+
+const composeEnhancers = compose(applyMiddleware(...middleWares));
+
+export const store = createStore(rootReducer, undefined, composeEnhancers);
